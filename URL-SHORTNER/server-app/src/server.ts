08d46c9e64d6723e55003ecb98dcecb3b644a4d1
vik1234-del <victor.okolo@pau.edu.ng>
2024-08-02 01:27:36 +0100

    create-api-endpoints

---
+++ URL-SHORTNER/server-app/src/server.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDb from "./config/dbConfig";
+import shortUrl from "./routes/shortUrl";
+
+dotenv.config();
+connectDb();
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "https://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use("/api/", shortUrl);
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
+});
