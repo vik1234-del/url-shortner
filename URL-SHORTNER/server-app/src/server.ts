9bc08a1a18ca3f9072697edda60884d760532454
Victor Chidubem Okolo <64779006+vik1234-del@users.noreply.github.com>
2024-08-16 15:53:15 +0100

    Update server.ts

--- URL-SHORTNER/server-app/src/server.ts
+++ URL-SHORTNER/server-app/src/server.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "hhttp://localhost:3000",
+    origin: "htttp://localhost:3000",
     credentials: true,
   })
 );
